d8f343140134ea9015253164686086e9fe3441ea
Marcio <dannydev.code@gmail.com>
2025-01-22 18:30:58 -0300

    renombre variables html

---
+++ js/diferecnciasVariables.js
@@ -0,0 +1,40 @@
+//var let const 
+// ambito / contexto - scope limites de las variables creadas 
+//var tiene alcance dentro de las funciones donde se declara
+//let tiene alcance solo dentro de la funcion donde se declaro y respeta esa regla var no
+//const es una variable inmutable, no puede cambiar su valor 
+
+//var 
+function ejemploVar() {
+    if(true) {
+        var numero = 98;
+    }
+    console.log(numero)
+}
+
+//ejemplo let
+function ejemploLet() {
+    if(true) {
+        let numero = 98;
+    }
+    console.log(numero)
+}
+
+//ejemplo const
+function ejemploConst() {
+    if(true) {
+        const a = 1;
+        a = 2;
+    }
+    console.log(a)
+}
+
+const array = [1, 2, 3];
+array.push(8);
+console.log(array);
+//array [] -> permite asignacion de datos pero no eliminacion de los datos inicializados
+
+
+ejemploVar();
+ejemploLet();
+ejemploConst();
--- js/variables.js
+++ js/variables.js
@@ -7,7 +7,8 @@ var persona1 = {
 let nombre = "Carlos Antonio Lopez";
 let costoPresupuesto = 98;
 const noCambia = false;
-
+console.log("5" + 1);
+console.log("5" - 1);
 console.log(typeof(persona1), persona1);
 console.log(typeof(nombre), nombre);
 console.log(typeof(costoPresupuesto), costoPresupuesto);
