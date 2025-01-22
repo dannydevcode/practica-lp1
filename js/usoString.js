a63f3fdcb59be0fc3a8af404660a4cea1890b3d0
Marcio <dannydev.code@gmail.com>
2025-01-22 19:50:38 -0300

    agregue array

--- js/usoString.js
+++ js/usoString.js
@@ -6,3 +6,12 @@ let comillaDoble = "texto en comillas dobles";
 const temPlate = `Parrafo construido usando las dos variables anteriores
 esas son ${comillaSimple} y ${comillaDoble}
 `;
+// para escape de caracteres se usa \" texto prueba\"
+//para agregar un "Enter" o  <br /> se usa \n 
+
+let autos = [
+    marca = 'toyota',
+    modelo= "toyo",
+    precio = 4500
+]
+console.log(autos)
