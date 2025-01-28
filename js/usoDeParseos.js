b2b929b035751af1a9bf6341f59d9a8a750dfa68
Marcio <dannydev.code@gmail.com>
2025-01-28 19:06:12 -0300

    practicas de DOM

---
+++ js/usoDeParseos.js
@@ -0,0 +1,15 @@
+//CONVERTIR DATO TIPO NUMERO A TIPO STRING
+let num1 = 3;
+let conver1 = String(num1);
+console.log(conver1, typeof(conver1));
+
+//CONVERTIR STRING A NUMERO
+let text2 = '5';
+let convert2 = Number(text2);
+console.log(convert2, typeof(text2));
+
+document.getElementsByTagName("p");
+document.getElementsByTagName("h1");
+ 
+console.log(document.getElementsByTagName("p"))
+
---
+++ js/usoDom.js
@@ -0,0 +1,12 @@
+
+
+
+const cambiarTitulo = () => {
+    const titulo1 = document.getElementById('titulo');
+    titulo1.outerText="nuevo titulo";
+    titulo1.style.color = 'red';
+}
+
+const cambiarParrafo = () => {
+    const parrafo = document.getElementById('');
+}
